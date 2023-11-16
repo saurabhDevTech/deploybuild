3f68693546ec40edf39fdbe2b9efe27139f29eeb
Dharmendra_singh1008 <dharmendra.shekhawat@devtechnosys.info>
2023-11-16 16:07:45 +0530

    skdf

--- functions/server.js
+++ functions/server.js
@@ -1,16 +1,7 @@
 exports.handler = async (event, context) => {
-    const express = require('express');
-    const app = express();
-
-    app.get('/', (req, res) => {
-        res.send('Hello, this is a basic Node.js project!');
-    });
-
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
-
+    function trialName() {
+        return "function working fine aksdj"
+    }
     return {
         statusCode: 200,
         body: JSON.stringify({ message: 'Serverless function executed!' }),
