5eea24c7721a40ec75ffcd8d00bf4217bc9cf886
Dharmendra_singh1008 <dharmendra.shekhawat@devtechnosys.info>
2023-11-16 16:37:21 +0530

    sdf

--- functions/expressApp.js
+++ functions/expressApp.js
@@ -3,31 +3,38 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello from the index route using Express!' });
+    res.status(200).json({ message: 'Hello from the index route using Express!' });
 });
 
 app.get('/about', (req, res) => {
-  res.status(200).json({ message: 'This is the about route using Express!' });
+    res.status(200).json({ message: 'This is the about route using Express!' });
 });
 
 app.get('/contact', (req, res) => {
-  res.status(200).json({ message: 'You\'ve reached the contact route using Express!' });
+    res.status(200).json({ message: 'You\'ve reached the contact route using Express!' });
 });
 
 // Creating a handler function that returns a promise
 const handler = async (event, context) => {
-  return new Promise((resolve, reject) => {
-    // Simulate the Express request and response objects
-    const req = { ...event };
-    const res = {
-      status: (statusCode) => ({
-        json: (data) => resolve({ statusCode, body: JSON.stringify(data) }),
-      }),
-    };
+    return new Promise((resolve, reject) => {
+        try {
+            // Simulate the Express request and response objects
+            const req = { ...event };
+            const res = {
+                status: (statusCode) => ({
+                    json: (data) => resolve({ statusCode, body: JSON.stringify(data) }),
+                }),
+            };
 
-    // Trigger the Express app using our simulated request and response
-    app(req, res);
-  });
+            // Trigger the Express app using our simulated request and response
+            app(req, res);
+        } catch (error) {
+            return {
+                statusCode: 200,
+                body: JSON.stringify({ message: 'here we go again' }),
+            };
+        }
+    });
 };
 
 // Export the handler function for Netlify Function
