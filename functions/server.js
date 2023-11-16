785d7e8b085d44bd7cc1e63554019059cadc75ac
Dharmendra_singh1008 <dharmendra.shekhawat@devtechnosys.info>
2023-11-16 16:18:35 +0530

    skdf

--- functions/server.js
+++ functions/server.js
@@ -4,6 +4,6 @@ exports.handler = async (event, context) => {
     }
     return {
         statusCode: 200,
-        body: JSON.stringify({ message: 'Serverless function executed!' }),
+        body: JSON.stringify({ message: 'Serverless function executed! hehe' }),
     };
 };
---
+++ functions/trialFun.js
@@ -0,0 +1,26 @@
+exports.handler = async (event, context) => {
+    const express = require('express');
+    const app = express();
+
+    app.get('/', (req, res) => {
+        res.send('Hello, this is a basic Node.js project!');
+    });
+    app.get('/second', (req, res) => {
+        res.send('no Hello!');
+    });
+
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: `Server running on port ${PORT}` }),
+        };
+    });
+    console.log("kjdsf")
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: 'in trial fun' }),
+    };
+};
